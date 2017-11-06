6bb29d94c24fd4d9196365f7bb01265284aed14d
Pradnya R Ambre <pra22@afsconnect1.njit.edu>
2017-11-05 22:34:26 -0500

    Database Connection

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+echo "<h1>Connecting to the database and retrieving value from the database</h1>";
+
+$db= new DatabaseConn();
+
+class DatabaseConn
+{
+    public $isCon;
+    protected $data;
+    
+    public function __construct($username = "pra22", $password = "zsLR8d2wM", $host = "sql2.njit.edu", $dbname = "pra22")
+    {
+        $this->isCon = TRUE;
+        try {
+                $this->data = new PDO("mysql:host={$host};dbname={$dbname};", $username, $password);
+                $this->data->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->data->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+                echo '<p> Connected to the Database successfully </p>' . '<br>';
+            }
+            catch (PDOException $e) 
+            {
+                $err_message = $e -> getMessage();
+                echo "<p> Error occurred while connecting to the database: $err_message </p>" . '<br>';
+            }
+        
+    }
+ 
+    public function Disconnect()
+    {
+        $this->data = NULL;
+        $this->isCon = FALSE;
+    }
+}
+
+?>
